71c0dbba028f372b78f9e4211696c14ec758d708
chihuynhminh <devchihuynhminh@gmail.com>
2021-01-29 16:28:21 +0700

    check loop nopae

--- ReadMemDump/main.py
+++ ReadMemDump/main.py
@@ -6,6 +6,7 @@ start_time = time.time()
 
 #file_mem_path = '../qemu_vm/x86/nopse_nopae/haikuOS/1'
 file_mem_path = '/media/n30/Data/qemu_vm/x86/nopae/haikuOS/memdumps/1'
+#file_mem_path = '/media/n30/Data/qemu_vm/x86/nopae/winxp/memdump/1'
 
 file = open(file_mem_path + '.memdump', 'rb')
 b = file.read()
--- ReadMemDump/scan_mem.py
+++ ReadMemDump/scan_mem.py
@@ -56,16 +56,32 @@ def scanPageTable(pde, pt_addr, pt_con_func, b, i0, i1, page_indexs_1, page_inde
             w += 1
     return w
 
+# Check is a page directory also a page table 
+def check_loop_pd(pd_addr, b, i0):
+    for i1 in range(1024):
+        pde_addr = pd_addr + i1 * 4
+        pde = getEntry(pde_addr, b, i0, i1, 0)
+        pt_addr = pde & 0xfffff000
+        if (pd_addr == pt_addr): return True
+    return False
+
 #scan page directory with pd_con_func is function check condition
 def scanPageDirectory(pd_addr, pd_con_func, pt_con_func, b, i0, page_indexs_1, page_indexs_2, mem_size, page_indexs_1_4MB = [], page_indexs_2_4MB = []):
     w = 0
+    isLoop = check_loop_pd(pd_addr, b, i0)
+
     for i1 in range(1024):
         pde_addr = pd_addr + i1 * 4
         pde = getEntry(pde_addr, b, i0, i1, 0)
 
         check_pde = pd_con_func(pde, mem_size)
         if (check_pde == 0): continue
-        if (check_pde == -1): return -1
+        if (check_pde == -1):
+            if(isLoop):
+                continue
+            else:
+                return -1
+            #return -1
 
         if (bitAt(pde, 7) == 1):
             page_addr = pde &0xffc00000
@@ -73,11 +89,20 @@ def scanPageDirectory(pd_addr, pd_con_func, pt_con_func, b, i0, page_indexs_1, p
                 w += 1
         else:
             pt_addr = pde & 0xfffff000
+            
             if (pt_addr > mem_size - 1024): continue
+            
             w_pte = scanPageTable(pde, pt_addr, pt_con_func, b, i0, i1, page_indexs_1, page_indexs_2)
-            if (w_pte == -1): return -1
+            
+            if (w_pte == -1):
+                if (isLoop):
+                    continue
+                else:
+                    return -1
+                #return -1
+            
             w += w_pte
-
+    
     return w
 
 # Find all 4MB page contain process name
